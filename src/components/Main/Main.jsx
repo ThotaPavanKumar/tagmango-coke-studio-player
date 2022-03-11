16ffe6925ca7daf3abc295933dae3fe590b72197
ThotaPavanKumar <pavankumarthota1@gmail.com>
2022-03-11 20:58:47 +0530

    displayed list of songs on DOM

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,24 +1,12 @@
 
 import React, { useState,useEffect } from 'react';
-import axios from "axios";
 import { Songs } from '../Songs/Songs';
+import styled from "./Main.module.css";
 
 export const Main = () => {
 
     const [list,setList] = useState([]);
 
-    // useEffect(() => {
-    //   let obj = {
-    //     url : "https://s3-ap-southeast-1.amazonaws.com/he-public-data/studiod9c0baf.json",
-    // };
-    //   axios(obj)
-    //   .then((result) =>  result.json())
-    //   .then((result) => {
-    //       console.log("result.data=",result)
-    //       setList(result.data)
-    //   })
-    //   .catch(e => console.log(e))   
-    // }, [])
     useEffect(() => {
         handleMusic();
       }, []);
@@ -31,11 +19,10 @@ export const Main = () => {
           .then((d) => {
             setList(d);
           });
-      };
-    
+      };    
 
   return (
-    <div>
+    <div className={styled.container}>
         {
            list.map((ele,index) => {
                console.log("ele=",ele)
--- src/components/Songs/Songs.jsx
+++ src/components/Songs/Songs.jsx
@@ -1,10 +1,33 @@
 
 import React from 'react'
+import styled from "./Song.module.css";
+import { BsPlayFill } from "react-icons/bs";
+
 
 export const Songs = ({data}) => {
     const {song,url,artists,cover_image} = data;
+
+    const handlePlay = (e) => {
+        console.log("e=",e);
+        
+    }
+
+
+
+
   return (
-    <div>{song}</div>
+    <div className={styled.songs}>
+        <div className={styled.image}>
+            <img src={cover_image} alt={song} />
+            <div className={styled.playbtn} onClick={() => handlePlay(data)}>
+                <BsPlayFill className={styled.playimg}/>
+            </div>
+            <div className={styled.text}>
+                <h3>{song}</h3>
+                <h4>{artists}</h4>
+            </div>
+        </div>
+    </div>
   )
 }
 
